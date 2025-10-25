0bbfd8f3f1b1bd35c182a66e4cf4ea0e6ed13673
consmarbella <46006507+consmarbella@users.noreply.github.com>
2025-10-25 00:53:17 -0300

    Fix corrupted private key - remove extra spaces

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDkgKNOKohjAsrV
 qWgdm2LxgVGJaxGw5csibJqFVUuHOPC9TqCdlVLsVmvXzSRJugTmB878S6hRMqOo
 rx9YotOMgTP/siQ7uAOT5JmlP8pkXBj9509MKSBXJ/HnAuYs55UJCJPRo2v1mKhZ
 UXGuRn7GZ3kw6h7Pb+HoyTDwN8RhXLnISvaQiwW5kEnzXFz10tfGMrruxwYIv7nT
-YLrtUUQ5tW/ACjBGISpaD cE1egQTmes1pXTQVuNDT1yaqzGmsnTmebR8lakfIwII
+YLrtUUQ5tW/ACjBGISpaDcE1egQTmes1pXTQVuNDT1yaqzGmsnTmebR8lakfIwII
 tfChGdMmcw6n31NM3l/vnBPb9ROk8zNsPqDeRBE0Mqf/FjnL45cfqLJOisT18SPl
 Ap0cW5g5AgMBAAECggEAHT24/Y/D1FJ50nh+B7aS0C+prNmJcgcgxPmmQdnLHeAo
 2TuZTOaHA2eBICSduDGYiYyw7ZMLqcMT3nT1S0p1oqKwx3qXLcKRR/kWZbyRZzZs
@@ -24,7 +24,7 @@ gKWdWAwzGJlYoteQE0Uj0OBx7OGbZwEeFGjKM2T8v2fx6fKXBvxqJo7YHz7+BCTv
 Fjw7GuoAKLJbtewAFT1NHHA7wblDQgZtpXoyNmLLqre8C4OalnTqLMvBKaUMJYcF
 JQd7TPqTnZ2VIUfv0vy4stQOXwKBgQDsMbZFCB2j0+XlSMmLwWeMX+hhMpsREqzb
 QIXf4C3mgE3Ww2EGuxR6waXB4AtqiUTKZGXKJtHobFo22tyHE/HWXs6osdibi2t3
-lqV6weL B7NIpVc6OhItghG/kX3NjoI3udqvvrrW7RnKLDHI/mxtwjCpgrwXFwFYG
+lqV6weLB7NIpVc6OhItghG/kX3NjoI3udqvvrrW7RnKLDHI/mxtwjCpgrwXFwFYG
 +pd3CKgwZwKBgF1Mooq3GIjIX+EymBpFq55v2gCxxqsmhZCoQcaxXKxucbtqfXhI
 dgWegO/aZ50x5grRPev7ZJq0grWM+CD9vmkBxZ+TtUVpbmPFnJdbzS+EVneo+uT5
 juQ1qthREvCLYeOtxJOnlobJWn0N8iSVA5GGuWs82G2i4QfO8xfdfpd/AoGAG/TC
@@ -77,11 +77,12 @@ app.post('/api/changelly', async (req, res) => {
     const data = await response.json();
     res.json(data);
   } catch (error) {
+    console.error('Error:', error);
     res.status(500).json({ error: error.message });
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-module.exports = app;
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
